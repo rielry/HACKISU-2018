17d40ca3116e369bed12546bf4e77d9b62d2bc90
Riley Jhi <riley.jhi@gmail.com>
2018-03-24 01:38:36 -0500

    fucked up api key whoops

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,20 +3,19 @@ function parseForm(e) {
     e.preventDefault();
 
     var location = $('#location').val();
-    var apiKey = 'AIzaSyD-HyEXFrceDw9DlR7Dq2umA7P7kNFV9V4';
+    var apiKey = 'AIzaSyDs3VmIxebVCXX0p1oH6vWjgias5-lorIQ';
     var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&key=' + apiKey;
  
     $.ajax({
         url: url,
         type: 'GET',
         dataType: 'JSON',
-        data: {},
         success: function(data) {
+
+            console.log(url);
             console.log(data);
         },
         error: function(err) {
-            console.log(url);
-            console.log('error');
             console.log(JSON.stringify(err));
         }
     });
