65b735bc323183eaa90c5fe095fba57a242fe058
Riley Jhi <riley.jhi@gmail.com>
2018-03-24 01:52:16 -0500

    figured out geocode holy fuck oh my god

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,18 +5,29 @@ function parseForm(e) {
     var location = $('#location').val();
     var apiKey = 'AIzaSyDs3VmIxebVCXX0p1oH6vWjgias5-lorIQ';
     var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&key=' + apiKey;
- 
-    $.ajax({
-        url: url,
-        type: 'GET',
-        dataType: 'JSON',
-        success: function(data) {
 
-            console.log(url);
-            console.log(data);
-        },
-        error: function(err) {
-            console.log(JSON.stringify(err));
-        }
-    });
+    var rs = '';
+    var loc;
+   
+    if(location) {
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'JSON',
+            success: function(data) {
+                if(data.results.length > 0) {
+                    loc = data.results[0].geometry.location;
+                    rs += loc.lat + ',' + loc.lng;
+                    return rs;
+                } else {
+                    //no matches
+                    return null;
+                }
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        });
+    }
+    return null;
 }
