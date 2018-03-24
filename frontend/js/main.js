88713fc8f127aee94d94ed1baedc8219b96f0c9b
Riley Jhi <riley.jhi@gmail.com>
2018-03-24 02:15:41 -0500

    more foolproof location detector

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,9 +1,8 @@
 function getLocation(e) {
     e.preventDefault();
-
     var location = $('#location').val();
     var apiKey = 'AIzaSyDs3VmIxebVCXX0p1oH6vWjgias5-lorIQ';
-    var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&key=' + apiKey;
+    var url = 'https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:' + location + '&components=postal_code&key=' + apiKey;
 
     var rs = '';
     var loc;
@@ -14,12 +13,14 @@ function getLocation(e) {
             type: 'GET',
             dataType: 'JSON',
             success: function(data) {
+                console.log(data.results);
                 if(data.results.length > 0) {
                     loc = data.results[0].geometry.location;
                     rs += loc.lat + ',' + loc.lng;
                     return rs;
                 } else {
                     //no matches
+                    alert('Uh oh! We couldn\'t find a location with zipcode ' + location + '! Try again?' );
                     return null;
                 }
             },
@@ -29,4 +30,8 @@ function getLocation(e) {
         });
     }
     return null;
+}
+
+function parseData() {
+
 }
