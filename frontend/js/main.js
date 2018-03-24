3ab03524d4d42b01de8b8ccb64aa3b905e9aa2cf
Riley Jhi <riley.jhi@gmail.com>
2018-03-24 00:48:56 -0500

    api request stufff

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,21 @@
-
-
 function parseForm() {
+
     var location = $('#location').val();
-    
+    var apiKey = 'AIzaSyD-HyEXFrceDw9DlR7Dq2umA7P7kNFV9V4';
+    var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&key=' + apiKey;
+ 
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'JSON',
+        data: {},
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(err) {
+            console.log(url);
+            console.log('error');
+            console.log(JSON.stringify(err));
+        }
+    });
 }
