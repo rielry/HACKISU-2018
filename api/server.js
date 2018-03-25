fc219caa639dea544eda23f9d556a217f41d6849
Zealot-311 <jackjohnson22@hotmail.com>
2018-03-24 23:15:46 -0500

    fixed cors issue

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
 'use strict';
 const yelpKey = 'K92teFVAnIt-SfOICRv2_YTrh7AFePL99a8SmxFl98LYnRPx793HtAPa3Rq_4Ch__Ldfln5kCpdkqG5E6XJ8YQ0f5aELNpcUBKBOjvJSmNKybTMEFAbb_ONQ0Q22WnYx'
 const yelp = require('yelp-fusion');
-
+const cors = require('cors');
 const client = yelp.client(yelpKey);
 const express        = require('express');
 
 const app            = express();
-
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 
